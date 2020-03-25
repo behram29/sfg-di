b30c501b2ab6edeea89cef7d595bcf5889bebbcb
Bahram Sattarli <mr.settarli97@gmail.com>
2020-03-25 21:38:29 +0400

    Second Commit: Initializin Spring Framework and making it Maven project

---
+++ src/test/java/bahram/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package bahram.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
