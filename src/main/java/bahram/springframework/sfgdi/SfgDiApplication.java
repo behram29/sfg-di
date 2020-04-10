b37c344379f9bdac6a1972d38a4c76daed744953
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-10 22:54:39 +0400

    12th Commit: Spring Multiple Property Files

--- src/main/java/bahram/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/bahram/springframework/sfgdi/SfgDiApplication.java
@@ -2,6 +2,7 @@ package bahram.springframework.sfgdi;
 
 import bahram.springframework.sfgdi.controllers.MyController;
 import bahram.springframework.sfgdi.examplebeans.FakeDataSource;
+import bahram.springframework.sfgdi.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -21,5 +22,8 @@ public class SfgDiApplication {
 
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUser());
+
+		FakeJmsBroker fakeJmsBroker =(FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
 	}
 }
--- src/main/java/bahram/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/bahram/springframework/sfgdi/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package bahram.springframework.sfgdi.config;
 
 import bahram.springframework.sfgdi.examplebeans.FakeDataSource;
+import bahram.springframework.sfgdi.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
 
     @Autowired
@@ -23,6 +29,13 @@ public class PropertyConfig {
     @Value("${bahram.dburl}")
     String url;
 
+    @Value("${bahram.jms.username}")
+    String jmsUsername;
+    @Value("${bahram.jms.password}")
+    String jmsPassword;
+    @Value("${bahram.jms.dburl}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -31,6 +44,16 @@ public class PropertyConfig {
         fakeDataSource.setUrl(url);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setPassword(jmsPassword);
+
+        return fakeJmsBroker;
     }
 
     @Bean
